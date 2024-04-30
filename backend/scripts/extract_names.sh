84aaf8838f83efb769804549a747a17af013928f
Robert L <rob_abcba@yahoo.com>
2024-04-30 15:45:45 -0700

    Update extract_names.sh

--- backend/scripts/extract_names.sh
+++ backend/scripts/extract_names.sh
@@ -1,3 +1,5 @@
 # extract_names.sh
 # grep -i "@amazon.com" ./Users\ list\ -\ MOCK_DATA.csv | cut -d ',' -f 2,3 > data.txt
-grep -i "@amazon.com" ./$1 | cut -d ',' -f 2,3 > $1.txt
+# grep -i "@amazon.com" ./$1 | cut -d ',' -f 2,3 > $1.txt
+# grep -i "@amazon.com" ./$1 | cut -d ',' -f 2,3 > output_names_$1.txt
+grep -i "@amazon.com" ./$1 | cut -d ',' -f 2,3 > output_names.txt
