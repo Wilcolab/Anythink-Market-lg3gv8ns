e9bc089060082c48bc50fbe9f9b82675469d09fb
Robert L <rob_abcba@yahoo.com>
2024-04-30 22:37:00 -0700

    Update extract_names.sh
    Swap order in cut command from lastfirst to firstlast.  Use sed to remove comma.

--- backend/scripts/extract_names.sh
+++ backend/scripts/extract_names.sh
@@ -2,4 +2,5 @@
 # grep -i "@amazon.com" ./Users\ list\ -\ MOCK_DATA.csv | cut -d ',' -f 2,3 > data.txt
 # grep -i "@amazon.com" ./$1 | cut -d ',' -f 2,3 > $1.txt
 # grep -i "@amazon.com" ./$1 | cut -d ',' -f 2,3 > output_names_$1.txt
-grep -i "@amazon.com" ./$1 | cut -d ',' -f 2,3 > output_names.txt
+# grep -i "@amazon.com" ./$1 | cut -d ',' -f 2,3 > output_names.txt
+grep -i "@amazon.com" ./$1 | cut -d ',' -f 3,2 | sed s/,//g > output_names.txt
